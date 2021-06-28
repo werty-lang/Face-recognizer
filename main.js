19215d74b2cda6052b1efd5d1e321cef321318e9
werty-lang <84279588+werty-lang@users.noreply.github.com>
2021-06-28 16:15:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,5 +19,20 @@ console.log('ml5 version:',ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Q397UzhaA/',modeLoaded);
 
 function modeLoaded(){
+console.log("modeLoaded")
+}
 
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult );
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        
+    }
 }
